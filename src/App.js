17cd4f8bca7f11d7995ace4b15586ed28f3d34b3
dongmin <rlaehdals753@naver.com>
2020-12-20 21:47:22 +0900

    props를 통해 컴포넌트에게 값 전달하기 + defaultProps, Wrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import Hello from './Hello';
+import React from "react";
+import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 function App() {
   return (
-    
-    <Hello />
+    <>
+      <Wrapper>
+        <Hello name="react" color="red" />
+        <Hello color="red" />
+      </Wrapper>
+    </>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
 
+/*  props 기본 사용법 */ 
 
-function Hello() {
-    return(
-        <div>안녕하세요~</div>
-    )
+/* function Hello(props) {
+  return (
+    <div
+      style={{
+        color: props.color,
+      }}
+    >
+      안녕하세요 {props.name}
+    </div>
+  );
+}  */
+
+/* 비구조할당으로 사용하는 방법  */
+
+/* function Hello(props) {
+  const { name, color } = props;
+  return <div style={{ color }}>안녕하세요 {name}</div>;
+} */
+
+ /* 비구조할당으로 사용하는 방법 2 */
+
+function Hello({ color, name }) {
+  return <div style={{ color }}>안녕하세요 {name}</div>;
+} */
+
+/* defaultProps로 기본값 설정   */
+function Hello({ color, name }) {
+  return (
+    <>
+      <div style={{ color }}> Hello, {name} </div>
+    </>
+  );
 }
-
+Hello.defaultProps = {
+  name: "이름없음",
+};
 export default Hello;
