ad3598e7d813696297c4c507f4b783b38dbfbad5
dongmin <rlaehdals753@naver.com>
2020-12-22 16:49:35 +0900

    배열 렌더링하기 - 수정

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+/* 한 파일에 여러개의 선언해도 무방 */
 function User({ user }) {
   return (
     <div>
@@ -28,6 +29,8 @@ function UserList() {
     },
   ];
 
+  /* 동적인 배열을 렌더링해야 할 때에는 자바스크립트
+  내장함수 map()을 사용합니다. */
   return (
     <div>
       {users.map((user) => (
