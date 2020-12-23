5cb05e912deb605acb8e926426450291f9041938
dongmin <rlaehdals753@naver.com>
2020-12-23 23:30:37 +0900

    배열에 항목 제거하기 - filter

--- src/App.js
+++ src/App.js
@@ -54,6 +54,10 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
   return (
     <div>
       <CreateUser
@@ -62,7 +66,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </div>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 /* 한 파일에 여러개의 선언해도 무방 */
-function User({ user }) {
+function User({ user, onRemove }) {
   return (
     <div>
-      <b>{user.username}</b>
-      <span>({user.useremail})</span>
+      <b>{user.username}</b> <span>({user.useremail})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove }) {
   /* 동적인 배열을 렌더링해야 할 때에는 자바스크립트
   내장함수 map()을 사용합니다. */
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} />
       ))}
     </div>
   );
