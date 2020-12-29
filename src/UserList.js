26d2ac30e0e04377de1baeb8ace0ac30d7d89c79
dongmin <rlaehdals753@naver.com>
2020-12-29 21:21:13 +0900

    useEffect - 언마운트

--- src/UserList.js
+++ src/UserList.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 /* 한 파일에 여러개의 컴포넌트 선언해도 무방 */
 function User({ user, onRemove, onToggle }) {
   //useEffect를 사용하여 마운트언마운트 업에이트시 할 작업 설정하기
-  //마운트->나타나는 것을 의미
+  //언마운트->사라지는 것을 의미
   //useEffect 이해 좀더 복습!++
   useEffect(() => {
     console.log("컴포넌트가 화면에 나타남");
