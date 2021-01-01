9ede2899795deb602e80df22fe0764d1d21ac1a6
dongmin <rlaehdals753@naver.com>
2021-01-01 23:14:18 +0900

    useEffect - 마무리 - 2

--- src/UserList.js
+++ src/UserList.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 /* 한 파일에 여러개의 컴포넌트 선언해도 무방 */
 function User({ user, onRemove, onToggle }) {
   //useEffect를 사용하여 마운트언마운트 업에이트시 할 작업 설정하기
-  //useEffect 마운트 언마운트 마무리 - 1
+  //useEffect 마운트 언마운트 마무리 - 2
   //useEffect 이해 좀더 복습!++
   useEffect(() => {
     console.log("컴포넌트가 화면에 나타남");
