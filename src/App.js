d42894a5b96c9cf85ce4663ca10773b7f6ee7c65
dongmin <rlaehdals753@naver.com>
2021-01-12 22:46:58 +0900

    useCallback을 사용하여 함수 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
@@ -15,13 +15,16 @@ function App() {
 
   const { username, useremail } = inputs;
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -44,7 +47,7 @@ function App() {
   ]);
   /* useRef로 컴포넌트 안의 변수 만들기 */
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -60,21 +63,27 @@ function App() {
     });
 
     nextId.current += 1;
-  };
+  }, [username, useremail, users]);
 
-  const onRemove = (id) => {
-    //user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id가 id 인 것을 제거함
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback(
+    (id) => {
+      //user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id가 id 인 것을 제거함
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
 
-  const onToggle = (id) => {
-    setUsers(
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
