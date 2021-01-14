5729dee6137bb7a15ee1bd412fa3b6712f94d516
dongmin <rlaehdals753@naver.com>
2021-01-14 22:58:13 +0900

    useReducer - 기초

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,26 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 /* useState를 통해 컴포넌트에서 바뀌는 값 관리하기 */
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
 function Counter() {
-  /* 배열 비구조화 할당을 통하여 각 원소를 추출 */
-  const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
 
-  /* 원래는 다음과 같이 해야함 
-  const numberState = useState(0);
-  const number = numberState[0];
-  const setNumber = numberState[1]; 
-  */
-
-  /* 그 다음 상태를 파라미터로 넣어줌 */
-  /* const onIncrease = () => {
-    setNumber(number + 1);
-  };
-  const onDcrease = () => {
-    setNumber(number - 1);
-  }; */
-
-  /* 함수형 업데이트 = 값을 업데이트 하는 함수를 파라미터로 넣어줌 => 최적화 */
   const onIncrease = () => {
-    setNumber((prevNumber) => prevNumber + 1);
+    dispatch({ type: "INCREMENT" });
   };
   const onDcrease = () => {
-    setNumber((prevNumber) => prevNumber - 1);
+    dispatch({ type: "DECREMENT" });
   };
 
   return (
