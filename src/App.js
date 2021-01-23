bc9392f1ba4290c6d13082e8b85b6f03661ba1f8
dongmin <rlaehdals753@naver.com>
2021-01-23 13:07:53 +0900

    Immer 를 사용한 더 쉬운 불변성 관리 - 마무리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useRef, useReducer, useCallback, useMemo } from "react";
+import React, { useReducer, useMemo } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
-import useInputs from "./hooks/useInputs";
 import produce from "immer";
 
 function countActiveUsers(users) {
@@ -37,22 +36,19 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
@@ -62,42 +58,18 @@ function reducer(state, action) {
 export const UserDispatch = React.createContext(null);
 
 function App() {
-  const [{ username, useremail }, onChange, onReset] = useInputs({
-    username: "",
-    useremail: "",
-  });
-
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
 
   const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: "CREATE_USER",
-      user: {
-        id: nextId.current,
-        username,
-        useremail,
-      },
-    });
-    nextId.current += 1;
-    onReset();
-  }, [username, useremail, onReset]);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <div>
-      <CreateUser
-        username={username}
-        useremail={useremail}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
       <div>활성 사용자 수 : {count}</div>
-    </div>
+    </UserDispatch.Provider>
   );
 }
 
