78586de2b37a77ceb96501306d945f65b33f5de2
dongmin <rlaehdals753@naver.com>
2021-01-24 20:21:31 +0900

    배운 내용 복습

--- src/App.js
+++ src/App.js
@@ -1,3 +1,80 @@
+/* import React, { useReducer, useMemo } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
+import produce from "immer";
+
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+
+const initialState = {
+  users: [
+    {
+      id: 1,
+      username: "dongmin",
+      useremail: "rlaehdals753@naver.com",
+      active: true,
+    },
+
+    {
+      id: 2,
+      username: "dongdong",
+      useremail: "rlaehdals224@naver.com",
+      active: false,
+    },
+
+    {
+      id: 3,
+      username: "kim",
+      useremail: "rlaehdals159@naver.com",
+      active: false,
+    },
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE_USER":
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    case "TOGGLE_USER":
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    case "REMOVE_USER":
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    default:
+      return state;
+  }
+}
+
+//UserDispatch 라는 이름으로 내보내줍니다.
+export const UserDispatch = React.createContext(null);
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { users } = state;
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
+      <div>활성 사용자 수 : {count}</div>
+    </UserDispatch.Provider>
+  );
+}
+
+export default App;
+ */
+
 import React, { useReducer, useMemo } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
