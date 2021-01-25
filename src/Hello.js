36b687c4f3e10ab33fa791c352346f85d2d8c7d8
dongmin <rlaehdals753@naver.com>
2021-01-25 21:21:05 +0900

    클래스형 컴포넌트 시작

--- src/Hello.js
+++ src/Hello.js
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-/* 조건부 렌더링 */
-function Hello({ color, name, isSpecial }) {
-  return (
-    <>
+class Hello extends Component {
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
       <div style={{ color }}>
-        {/* {isSpecial ? <b>*</b> : null}  
-        삼항연산자를 사용한 조건부 렌더링은 주로 특정 조건에 따라
-        내용이 다를 때 사용 현재는 그냥 보여주고 숨기는 용도
-        이기 때문에 하단의 코드를 사용하면 효율적*/}
         {isSpecial && <b>*</b>}
-        Hello, {name}
+        안녕하세요 {name}
       </div>
-    </>
-  );
+    );
+  }
 }
+
 Hello.defaultProps = {
   name: "이름없음",
 };
+
 export default Hello;
