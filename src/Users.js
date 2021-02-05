e7867f68532bd75549286130b93a4fba7f5cdb6c
dongmin <rlaehdals753@naver.com>
2021-02-05 16:50:55 +0900

    API 연동 - API 에 파라미터가 필요한 경우

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import useAsync from "./useAsync";
+import User from "./User";
 
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
 // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
@@ -12,6 +13,7 @@ async function getUsers() {
 }
 
 function Users() {
+  const [userId, setUserId] = useState(null);
   const [state, refetch] = useAsync(getUsers, [], true);
 
   const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
@@ -23,12 +25,17 @@ function Users() {
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: "pointer" }}
+          >
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
       <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
