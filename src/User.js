bba63b38b511e25104fad687a8fd52226245d15c
dongmin <rlaehdals753@naver.com>
2021-02-06 19:51:02 +0900

    API 연동 - react-async 로 요청 상태 관리하기

--- src/User.js
+++ src/User.js
@@ -1,8 +1,8 @@
 import React from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 
-async function getUser(id) {
+async function getUser({ id }) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
@@ -10,10 +10,13 @@ async function getUser(id) {
 }
 
 function User({ id }) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  const { data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
+  });
 
-  if (loading) return <div>로딩중..</div>;
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 import User from "./User";
 
-// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
-// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
 async function getUsers() {
   const response = await axios.get(
     "https://jsonplaceholder.typicode.com/users"
@@ -14,13 +12,14 @@ async function getUsers() {
 
 function Users() {
   const [userId, setUserId] = useState(null);
-  const [state, refetch] = useAsync(getUsers, [], true);
+  const { data: users, error, isLoading, run } = useAsync({
+    deferFn: getUsers,
+  });
 
-  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
-
-  if (loading) return <div>로딩중..</div>;
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
+
   return (
     <>
       <ul>
@@ -34,7 +33,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={run}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
